a423c25ce345b04a18cd2f7fa5979d131595a9df
Bartomeu <bartomeutg@gmail.com>
2023-05-30 21:54:33 +0200

    Module 3 finished.

--- app.js
+++ app.js
@@ -1,51 +1,8 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
+console.log(routes.someTest);
 
-    console.log(req.url, req.method, req.headers);
-
-    const url = req.url;
-    const method = req.method;
-
-    if (url === '/') {
-        res.setHeader('Content-Type', 'text/html');
-
-        res.write('<html>');
-        res.write('<header><title>Enter message</title></header>')
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-
-        return res.end();
-    }
-
-    if (url === '/message' && method === 'POST') {
-
-        const body = [];
-
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-
-            fs.writeFile('message.txt', message, (err) => {
-                res.writeHead(302, { 'Location': '/' });
-                return res.end();
-            });
-        });
-    }
-
-    res.setHeader('Content-Type', 'text/html');
-
-    res.write('<html>');
-    res.write('<header><title>My First page</title></header>')
-    res.write('<body><h1>Hello from my Node.js server;</h1></body>');
-    res.write('</html>');
-
-    res.end();
-});
+const server = http.createServer(routes.handler);
 
 server.listen(8080);
