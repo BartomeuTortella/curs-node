1a5a28b13daed7d628b7580d0f8a7b21f2da2865
Bartomeu <bartomeutg@gmail.com>
2023-06-01 18:46:23 +0200

    WIP

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+const shopRoutes = require('./routes/shop');
+const exp = require('constants');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
